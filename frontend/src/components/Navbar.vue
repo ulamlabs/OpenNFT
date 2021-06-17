04c521a700ab4e362f0c10e18f4c8a787179dbef
Patryk Grzyb <gpatryk14@gmail.com>
2021-06-17 14:19:23 +0200

    Fix missing links

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -82,7 +82,7 @@
                 {{ entry.label }}
               </router-link>
               <a
-                href="#"
+                href="https://www.ulam.io/case-studies/opennft-whitelabel-nft-marketplace/"
                 target="blank"
                 class="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-base font-medium"
               >
